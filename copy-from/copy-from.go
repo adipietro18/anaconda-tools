b232113da54c775e1153128f35c259197a031a64
Anthony DiPietro <51377278+adipietro18@users.noreply.github.com>
2022-01-12 19:14:22 -0500

    Large refactor of copy-from tools. (#3)

--- copy-from/copy-from.go
+++ copy-from/copy-from.go
@@ -10,44 +10,88 @@ import (
 	"strings"
 )
 
-func buildRsyncCommand(platform string, dryRun bool) (*exec.Cmd, error) {
-	upperPlatform := strings.ToUpper(platform)
+type CopyFromParameters struct {
+	DryRun   bool
+	Platform string
+}
 
+type SshLocation struct {
+	User string
+	Host string
+	Path string
+}
+
+func RunCopyFrom(parameters CopyFromParameters, source SshLocation) error {
+	var commandBuilder strings.Builder
+	commandBuilder.WriteString("rsync -amv")
+	if parameters.DryRun {
+		commandBuilder.WriteString(" --dry-run")
+	}
+	commandBuilder.WriteString(fmt.Sprintf(" --include='linux-%s/'", parameters.Platform))
+	commandBuilder.WriteString(" --include='*.conda'")
+	commandBuilder.WriteString(" --include='*.tar.bz2'")
+	commandBuilder.WriteString(" --exclude='*'")
+	commandBuilder.WriteString(fmt.Sprintf(" %s@%s:%s", source.User, source.Host, source.Path))
+	commandBuilder.WriteString(" ./")
+
+	command := exec.Command("bash", "-c", commandBuilder.String())
+
+	var outBytes, errBytes bytes.Buffer
+	command.Stdout = &outBytes
+	command.Stderr = &errBytes
+
+	fmt.Println("Running: " + command.String())
+
+	err := command.Run()
+
+	if err != nil {
+		fmt.Println(errBytes.String())
+		return err
+	}
+
+	fmt.Println(outBytes.String())
+
+	return nil
+}
+
+func ValidateParameters(parameters CopyFromParameters) error {
+	err := validatePlatform(parameters.Platform)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getSshLocation(platform string) (SshLocation, error) {
+	upperPlatform := strings.ToUpper(platform)
 	sshUser := os.Getenv(upperPlatform + "_SSH_USER")
 	if sshUser == "" {
-		return nil, errors.New(upperPlatform + "_SSH_USER environment variable must be defined.")
+		return SshLocation{}, errors.New(upperPlatform + "_SSH_USER environment variable must be defined.")
 	}
 
 	sshHost := os.Getenv(upperPlatform + "_SSH_HOST")
 	if sshHost == "" {
-		return nil, errors.New(upperPlatform + "_SSH_HOST environment variable must be defined.")
+		return SshLocation{}, errors.New(upperPlatform + "_SSH_HOST environment variable must be defined.")
 	}
 
 	sshPath := os.Getenv(upperPlatform + "_SSH_PATH")
 	if sshPath == "" {
-		return nil, errors.New(upperPlatform + "_SSH_PATH environment variable must be defined.")
+		return SshLocation{}, errors.New(upperPlatform + "_SSH_PATH environment variable must be defined.")
 	}
 
-	command := []string{"-amv"}
-	if dryRun {
-		command = append(command, "--dry-run")
-	}
-	command = append(command, fmt.Sprintf("--include='linux-%s'", platform))
-	command = append(command, "--include='*.conda'")
-	command = append(command, "--include='*.tar.bz2'")
-	command = append(command, "--exclude='*'")
-	command = append(command, fmt.Sprintf("%s@%s:%s", sshUser, sshHost, sshPath))
-	command = append(command, "./")
-
-	return exec.Command("rsync", command...), nil
+	return SshLocation{User: sshUser, Host: sshHost, Path: sshPath}, nil
 }
 
-func parsePlatform(platform string) (string, error) {
+func normalizePlatform(platform string) string {
+	return strings.ToLower(platform)
+}
+
+func validatePlatform(platform string) error {
 	switch platform {
 	case "s390x", "aarch64":
-		return platform, nil
+		return nil
 	default:
-		return "", errors.New("the provided platform must be either s390x or aarch64")
+		return errors.New("the provided platform is not one of the expected values")
 	}
 }
 
@@ -91,33 +135,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	platform, err := parsePlatform(flag.Arg(0))
+	parameters := CopyFromParameters{DryRun: dryRun, Platform: normalizePlatform(flag.Arg(0))}
+
+	err := ValidateParameters(parameters)
 	if err != nil {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	rsyncCommand, err := buildRsyncCommand(platform, dryRun)
+	source, err := getSshLocation(parameters.Platform)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	var outBytes, errBytes bytes.Buffer
-	rsyncCommand.Stdout = &outBytes
-	rsyncCommand.Stderr = &errBytes
-
-	fmt.Println("Running: " + rsyncCommand.String())
-
-	err = rsyncCommand.Run()
-
-	if err != nil {
-		fmt.Println(errBytes.String())
+	if err = RunCopyFrom(parameters, source); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	fmt.Println(outBytes.String())
 
 	os.Exit(0)
 }
